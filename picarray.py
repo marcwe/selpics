061e863fcfdad986f8a6c7bc8bd4bb7bd69191d6
marcwe <marcwe@pcom.edu>
2016-07-07 18:40:54 -0400

    Update picarray.py
    add  lines 31 to 38  size select and event toggle

--- picarray.py
+++ picarray.py
@@ -28,7 +28,14 @@ myList = [q for q in ifile.readlines()]
 # top = '<html><head><title>Graduate Commencement 2014</title></head><body><table><tr><td colspan=5><H3 align="center">Graduate Commencement 2014</H3></td></tr><tr><td colspan=4></td><td><H5 align="center"><a href="http://dev.pcom.edu/pcomweb/marcwe/grad_hooding_2014/grad_hooding_2014.html"></a></H5></td></tr><tr>'
 
 top = '<html><head><title>' + title + '</title></head><body><p align="center"><H3 align="center">' + title + '</H3></p>'
+hoodselect = '<p><H4 style = "text-align:right;margin-right:100px;"><a href="thumb15.html">Graduation Images</a></H4></p><p><H5 style = "margin-left:60px;"><a href="thumb-hood10.html">Very Small</a> &nbsp;<a href="thumb-hood15.html">Small</a>  &nbsp;<a href="thumb-hood20.html">Medium</a>  &nbsp;<a href="thumb-hood30.html">Large</a></H5></p>'
+commselect = '<p><H4 style = "text-align:right;margin-right:100px;"><a href="thumb-hood20.html">Hooding Images</a></H4></p><p><H5 style = "margin-left:60px;"><a href="thumb10.html">Small</a> &nbsp;<a href="thumb15.html">Medium</a>  &nbsp;<a href="thumb20.html">Large</a></H5></p>'
+
 f.write(top)
+if len(thumbsize) == 12:
+    f.write(hoodselect)
+else:
+    f.write(commselect)
 i = 0
 while i < len(myList):
     print myList[i]
